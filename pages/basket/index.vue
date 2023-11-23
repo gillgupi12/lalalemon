bd224e883c1b2cc394ae0fd36ba13101afc46496
gurpindersinghgill <gupigill12@gmail.com>
2023-11-23 12:09:04 +0800

    feat: add empty basket page

--- pages/basket/index.vue
+++ pages/basket/index.vue
@@ -1,7 +1,13 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div>Basket</div>
+  <UContainer class="max-w-4xl p-10 items-center flex flex-col gap-4">
+    <Icon name="ph:shopping-cart" size="80" />
+    <p class="text-3xl font-bold">Your bag is empty</p>
+    <p>Not sure where to start? Here are some of our favorites.</p>
+    <div class="flex flex-row gap-3">
+      <UButton color="black" label="SHOP WOMEN'S" class="rounded py-4 px-8" />
+      <UButton color="black" label="SHOP MEN'S" class="rounded py-4 px-8" />
+    </div>
+  </UContainer>
 </template>
