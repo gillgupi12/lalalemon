3788b2e1c30eef0f5eca98e8984b22ed061897eb
gurpindersinghgill <gupigill12@gmail.com>
2023-11-24 15:19:41 +0800

    feat: add reuseable filtering component

---
+++ components/organisms/gender/filter.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const emits = defineEmits(["filter", "nofilter"]);
+
+const loading = ref(false);
+const allCategories = ref();
+const fetchCategories = async () => {
+  loading.value = true;
+  const { data, error } = await supabase.from("categories").select();
+
+  if (data) {
+    allCategories.value = data.map((category: any) => {
+      return {
+        ...category,
+        selected: false,
+      };
+    });
+  }
+  loading.value = false;
+};
+
+onMounted(async () => {
+  await fetchCategories();
+});
+
+watch(
+  allCategories,
+  async () => {
+    const data = allCategories.value
+      .filter((category: any) => category.selected === true)
+      .map((category: any) => category.id);
+
+    if (data.length === 0) {
+      emits("nofilter");
+      //   await fetchProducts();
+    } else {
+      emits("filter", data);
+      //   await fetchProductswithFilters(data);
+    }
+  },
+  { deep: true, immediate: false }
+);
+
+const items = [
+  {
+    label: "Category",
+    icon: "i-heroicons-information-circle",
+    defaultOpen: true,
+    slot: "category-filter",
+  },
+];
+</script>
+
+<template>
+  <div>
+    <div class="text-sm p-4">
+      <div>Filters</div>
+      <div>No filters applied</div>
+    </div>
+    <UAccordion :items="items" class="bg-white border">
+      <template #category-filter>
+        <div class="flex flex-col space-y-1 px-4">
+          <div v-for="(categories, index) in allCategories" :key="index">
+            <UCheckbox
+              v-model="categories.selected"
+              name="categories"
+              class="capitalize"
+              :label="categories.name"
+            >
+            </UCheckbox>
+          </div>
+        </div>
+      </template>
+
+      <template #default="{ item, index, open }">
+        <UButton
+          color="gray"
+          variant="ghost"
+          class="border-b border-gray-200 dark:border-gray-700 hover:bg-white"
+          :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }"
+        >
+          <span class="truncate"> {{ item.label }}</span>
+
+          <template #trailing>
+            <UIcon
+              name="i-heroicons-chevron-right-20-solid"
+              class="w-5 h-5 ms-auto transform transition-transform duration-200"
+              :class="[open && 'rotate-90']"
+            />
+          </template>
+        </UButton>
+      </template>
+    </UAccordion>
+  </div>
+</template>
--- pages/men/best-sellers.vue
+++ pages/men/best-sellers.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ProductCard from "components/organisms/product/card.vue";
+import ProductFilter from "components/organisms/gender/filter.vue";
 const supabase = useSupabaseClient();
 
 const allProducts = ref();
@@ -15,6 +16,19 @@ const fetchProducts = async () => {
   loading.value = false;
 };
 
+const fetchProductswithFilters = async (categoryIds: number) => {
+  loading.value = true;
+  const { data, error } = await supabase
+    .from("products")
+    .select()
+    .in("category_id", [categoryIds]);
+
+  if (data) {
+    allProducts.value = data;
+  }
+  loading.value = false;
+};
+
 onMounted(async () => {
   await fetchProducts();
 });
@@ -23,8 +37,15 @@ onMounted(async () => {
 <template>
   <div>
     <UProgress animation="carousel" v-if="loading" size="sm" color="red" />
-    <UContainer>
-      <div class="grid grid-cols-3 gap-4 py-20 max-w-fit">
+
+    <UContainer class="grid grid-cols-4 py-20 gap-4">
+      <div class="col-span-1">
+        <ProductFilter
+          @filter="fetchProductswithFilters"
+          @nofilter="fetchProducts()"
+        />
+      </div>
+      <div class="grid grid-cols-3 gap-4 max-w-fit col-span-3">
         <div v-for="(product, index) of allProducts" :key="index">
           <ProductCard :item="product" />
         </div>
