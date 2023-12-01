d16e8818f9cfc28f8ed48a920d6a7a4739784eb1
gurpindersinghgill <gupigill12@gmail.com>
2023-12-02 03:18:34 +0800

    fix: correct of if statements

--- pages/basket/index.vue
+++ pages/basket/index.vue
@@ -110,90 +110,95 @@ const breakpoints = {
 </script>
 
 <template>
-  <div v-if="!loading">
-    <UContainer
-      v-if="basketData?.length === 0"
-      class="max-w-4xl p-10 items-center flex flex-col gap-4 py-40"
-    >
-      <Icon name="ph:shopping-cart" size="80" />
-      <p class="text-3xl font-bold">Your bag is empty</p>
-      <p>Not sure where to start? Here are some of our favorites.</p>
-      <div class="flex flex-row gap-3">
-        <UButton
-          color="black"
-          label="SHOP WOMEN'S"
-          class="rounded py-4 px-8"
-          @click="() => router.push('/women')"
-        />
-        <UButton
-          color="black"
-          label="SHOP MEN'S"
-          class="rounded py-4 px-8"
-          @click="() => router.push('/men')"
-        />
-      </div>
-    </UContainer>
-    <UContainer v-else class="flex flex-col gap-4 py-10">
-      <div class="flex flex-row justify-between">
-        <p class="md:text-3xl font-bold">My Bag({{ basketData?.length }})</p>
-        <p class="md:text-xl font-bold">Need Help? +852 8009 06220</p>
-      </div>
-      <UDivider />
-      <div class="md:grid grid-cols-3 gap-4">
-        <div class="col-span-2 w-full pb-2 md:pb-0">
-          <div v-for="item in basketData">
-            <ProductCheckoutCard
-              :item="item.item_data"
-              @refetch-basket="loadBasketData()"
+  <div>
+    <div v-if="loading" class="flex items-center justify-center py-40">
+      <div class="font-bold text-4xl">Loading Basket...</div>
+    </div>
+    <div v-else>
+      <UContainer
+        v-if="basketData?.length > 0"
+        class="flex flex-col gap-4 py-10"
+      >
+        <div class="flex flex-row justify-between">
+          <p class="md:text-3xl font-bold">My Bag({{ basketData?.length }})</p>
+          <p class="md:text-xl font-bold">Need Help? +852 8009 06220</p>
+        </div>
+        <UDivider />
+        <div class="md:grid grid-cols-3 gap-4">
+          <div class="col-span-2 w-full pb-2 md:pb-0">
+            <div v-for="item in basketData">
+              <ProductCheckoutCard
+                :item="item.item_data"
+                @refetch-basket="loadBasketData()"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <div class="flex flex-row justify-between">
+              <p>Subtotal</p>
+              <div>HK$1,180.00</div>
+            </div>
+            <UDivider />
+            <div class="flex flex-row justify-between">
+              <p>Shipping cost</p>
+              <div>FREE</div>
+            </div>
+            <UDivider />
+            <div class="flex flex-row justify-between font-bold">
+              <div>Estimated Total</div>
+              <div>HK$1,180.00</div>
+            </div>
+            <UButton
+              color="red"
+              class="py-4 rounded-none text-center flex items-center justify-center"
+              label="CHECKOUT"
             />
           </div>
         </div>
-        <div class="flex flex-col gap-2">
-          <div class="flex flex-row justify-between">
-            <p>Subtotal</p>
-            <div>HK$1,180.00</div>
-          </div>
-          <UDivider />
-          <div class="flex flex-row justify-between">
-            <p>Shipping cost</p>
-            <div>FREE</div>
-          </div>
-          <UDivider />
-          <div class="flex flex-row justify-between font-bold">
-            <div>Estimated Total</div>
-            <div>HK$1,180.00</div>
-          </div>
+      </UContainer>
+      <UContainer
+        v-else
+        class="max-w-4xl p-10 items-center flex flex-col gap-4 py-40"
+      >
+        <Icon name="ph:shopping-cart" size="80" />
+        <p class="text-3xl font-bold">Your bag is empty</p>
+        <p>Not sure where to start? Here are some of our favorites.</p>
+        <div class="flex flex-row gap-3">
           <UButton
-            color="red"
-            class="py-4 rounded-none text-center flex items-center justify-center"
-            label="CHECKOUT"
+            color="black"
+            label="SHOP WOMEN'S"
+            class="rounded py-4 px-8"
+            @click="() => router.push('/women')"
+          />
+          <UButton
+            color="black"
+            label="SHOP MEN'S"
+            class="rounded py-4 px-8"
+            @click="() => router.push('/men')"
           />
         </div>
-      </div>
-    </UContainer>
-    <UContainer class="flex flex-col gap-8 py-4">
-      <div>
-        <p class="text-4xl text-center font-bold">You May Also Like</p>
-      </div>
-      <div class="py-10">
-        <carousel
-          :wrapAround="true"
-          snapAlign="start"
-          :breakpoints="breakpoints"
-        >
-          <slide v-for="product in allProducts" :key="product">
-            <ProductCard :item="product" />
-          </slide>
+      </UContainer>
+      <UContainer class="flex flex-col gap-8 py-4">
+        <div>
+          <p class="text-4xl text-center font-bold">You May Also Like</p>
+        </div>
+        <div class="py-10">
+          <carousel
+            :wrapAround="true"
+            snapAlign="start"
+            :breakpoints="breakpoints"
+          >
+            <slide v-for="product in allProducts" :key="product">
+              <ProductCard :item="product" />
+            </slide>
 
-          <template #addons>
-            <navigation />
-            <pagination />
-          </template>
-        </carousel>
-      </div>
-    </UContainer>
-  </div>
-  <div v-else class="flex items-center justify-center py-40">
-    <div class="font-bold text-4xl">Loading Basket...</div>
+            <template #addons>
+              <navigation />
+              <pagination />
+            </template>
+          </carousel>
+        </div>
+      </UContainer>
+    </div>
   </div>
 </template>
