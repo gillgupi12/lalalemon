4ca00c7b8f263c84d1cd5d9affa8f006d955fba8
gurpindersinghgill <gupigill12@gmail.com>
2023-12-05 13:18:28 +0800

    feat: add page transition

--- app.vue
+++ app.vue
@@ -42,4 +42,18 @@ useHead({
 .page-leave-to {
   opacity: 0;
 }
+
+.layout-enter-from {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.2s;
+}
+.layout-enter,
+.layout-leave-to {
+  opacity: 0;
+}
 </style>
